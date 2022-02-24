86cbf13b4b792c22a8595f9355dfdfd2b081a168
Nguyen <nguyentrung.nguyen@dxc.com>
2022-02-25 00:10:20 +0700

    Add collections to firebase database and handle get the collections data from firestore

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
-import { auth, createUserProfileDocument } from './firebase/firebase.util';
+import { auth, createUserProfileDocument, addCollectionAndDocument } from './firebase/firebase.util';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPageComponent from './pages/shop/shop.component';
@@ -13,13 +13,15 @@ import HeaderComponent from './components/header/header.component';
 import SignInAndSignUpComponent from './pages/signin-and-signup/signin-and-signup.component';
 import { setCurrentUser } from '../src/redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selector';
+import { selectCollectionsForPreview } from './redux/shop/shop.selector';
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
 
-    const { setCurrentUser } = this.props;
+    const { setCurrentUser, collectionsArray } = this.props;
+
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
@@ -29,11 +31,11 @@ class App extends React.Component {
             id: snapShot.id,
             ...snapShot.data()
           })
-          console.log(this.state);
         });
       }
 
       setCurrentUser({ userAuth });
+      addCollectionAndDocument('collections', collectionsArray);
     });
   }
 
@@ -48,7 +50,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPageComponent} />
-          <Route
+          {/* <Route
             exact
             path='/signin'
             render={() =>
@@ -58,7 +60,9 @@ class App extends React.Component {
                 <SignInAndSignUpComponent />
               )
             }
-          />
+          /> */}
+          <Route
+            exact path='/signin' component={SignInAndSignUpComponent} />
           <Route exact path='/checkout' component={CheckOutComponent} />
         </Switch>
       </div>
@@ -67,7 +71,8 @@ class App extends React.Component {
 }
 
 const mapStateToProps = createStructuredSelector({
-  currentUser: selectCurrentUser
+  currentUser: selectCurrentUser,
+  collectionsArray: selectCollectionsForPreview
 });
 
 const mapDispatchToProps = dispatch => ({
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,14 +1,38 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import CollectionPage from '../collection/collection.component';
+import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.util';
+import { updateCollections } from '../../redux/shop/shop.action';
 
 
-const ShopPageComponent = ({  match }) => (
-    <div className='shop-page'>
-        <Route exact path={`${match.path}`} component={CollectionsOverview} />
-        <Route exact path={`${match.path}/:collectionId`} component={CollectionPage} />
-    </div>
-);
+class ShopPageComponent extends React.Component {
+    unsubscribeFromSnapShop = null;
+
+    componentDidMount() {
+        const { updateCollections } = this.props;
+        const collectionRef = firestore.collection('collections');
+
+        collectionRef.onSnapshot(async snapshot => {
+            const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+            updateCollections(collectionsMap);
+        })
+    }
+
+    render() {
+        const { match } = this.props;
+        return (
+            <div className='shop-page'>
+                <Route exact path={`${match.path}`} component={CollectionsOverview} />
+                <Route exact path={`${match.path}/:collectionId`} component={CollectionPage} />
+            </div>
+        )
+    }
+};
+
+const mapDispatchToProps = dispatch => ({
+    updateCollections: collectionsMap => dispatch(updateCollections(collectionsMap))
+});
    
-export default ShopPageComponent;
+export default connect(null, mapDispatchToProps) (ShopPageComponent);
